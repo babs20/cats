1466efd947e65d7220cfbfa2726bd38b6da27fc0
Brady <thevillemc@gmail.com>
2021-01-12 17:18:55 +0000

    added testing for async functions

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,13 +1,21 @@
 // asyncBreeds.js
 const fs = require('fs');
 
-const breedDetailsFromFile = function (breed, callback) {
-  console.log('breedDetailsFromFile: Calling readFile...');
+const breedDetailsFromFile = function (breed, done) {
+  // console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log("In readFile's Callback: it has the data.");
-    if (!error) callback(data);
+    // console.log("In readFile's Callback: it has the data.");
+    if (!error) {
+      done(data);
+    } else {
+      done(undefined);
+    }
   });
 };
 
+// const done = (data) => console.log('Return value: ', data);
+
+module.exports = breedDetailsFromFile;
+
 // we try to get the return value
-const bombay = breedDetailsFromFile('Bombay', (data) => console.log('Return value: ', data));
+// const bombay = breedDetailsFromFile('Bombay', (data) => console.log('Return value: ', data));
